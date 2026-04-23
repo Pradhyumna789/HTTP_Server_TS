5b3bf0e5a564618c0bd67facb9dd51448a3b511a
Pradhyumna789 <hariharshasripradhyumna@gmail.com>
2026-04-23 10:13:52 +0000

    refactored a bit and implemented inheritance based error handling

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,47 @@ import express from "express";
 import { config } from "./config.js";
 const app = express();
 const PORT = 8080;
+app.use(express.json());
+class badRequestError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+class unauthorizedError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+class forbiddenError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+class notFoundError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+const middlewareErrorHandler = (err, req, res, next) => {
+    if (err instanceof badRequestError) {
+        res.status(400).json({ error: err.message });
+    }
+    else if (err instanceof notFoundError) {
+        console.log(err.message);
+        res.status(404).json({ error: "Nothing found on this resource" });
+    }
+    else if (err instanceof unauthorizedError) {
+        console.log(err.message);
+        res.status(401).json({ error: "You are unauthorized to access this resource" });
+    }
+    else if (err instanceof forbiddenError) {
+        console.log(err.message);
+        res.status(403).json({ error: "You don't have the sufficient rights to access this resource" });
+    }
+    else {
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
 const middlewareLogResponses = (req, res, next) => {
     res.on("finish", () => {
         if (res.statusCode !== 200) {
@@ -14,7 +55,7 @@ const middlewareMetricsInc = (req, res, next) => {
     config.fileServerHits++;
     next();
 };
-const validateChirpHandler = (req, res) => {
+const validateChirpHandler = async (req, res) => {
     let parsedBody;
     parsedBody = req.body;
     if (parsedBody.body.length <= 140) {
@@ -27,20 +68,16 @@ const validateChirpHandler = (req, res) => {
             }
         }
         const response = {
-            cleanedBody: words.join(" "),
+            cleanedBody: words.join(" ")
         };
         res.status(200).json(response);
     }
     else {
-        const response = {
-            error: "Chirp is too long",
-        };
-        res.status(400).json(response);
+        throw new badRequestError("Chirp is too long. Max length is 140");
     }
 };
 app.use("/app", middlewareMetricsInc, express.static("./src/app/"));
 app.use(middlewareLogResponses);
-app.use(express.json());
 app.get("/app", (req, res) => {
     res.set("Cache-Control", "no-cache");
     res.send("Welcome to Chirpy");
@@ -60,11 +97,19 @@ app.get("/admin/metrics", (req, res) => {
     </html>
   `);
 });
-app.post("/api/validate_chirp", validateChirpHandler);
+app.post("/api/validate_chirp", async (req, res, next) => {
+    try {
+        await validateChirpHandler(req, res);
+    }
+    catch (err) {
+        next(err);
+    }
+});
 app.post("/admin/reset", (req, res) => {
     config.fileServerHits = 0;
     res.status(200).end();
 });
+app.use(middlewareErrorHandler);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,125 @@
-import express, { Request, Response, NextFunction } from "express";
-import { config } from "./config.js";
+import express, { Request, Response, NextFunction } from "express"
+import { config } from "./config.js"
 
-const app = express();
-const PORT = 8080;
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+
+class badRequestError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+class unauthorizedError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+class forbiddenError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+class notFoundError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+const middlewareErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof badRequestError) {
+    res.status(400).json({ error: err.message })
+  } else if(err instanceof notFoundError) {
+    console.log(err.message)
+    res.status(404).json({ error: "Nothing found on this resource" })
+  } else if(err instanceof unauthorizedError) {
+    console.log(err.message)
+    res.status(401).json({ error: "You are unauthorized to access this resource" })
+  } else if(err instanceof forbiddenError) {
+    console.log(err.message)
+    res.status(403).json({ error: "You don't have the sufficient rights to access this resource" })
+  } else {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
 
 const middlewareLogResponses = (req: Request, res: Response, next: NextFunction): void => {
   res.on("finish", () => {
     if(res.statusCode !== 200) {
-      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
     }
-  });
+  })
 
-  next();
-};
-
-res.on("finish", () => {
-  console.log(`[NON-OK] ${res}`);
-})
-
-const middlewareMetricsInc = (req: Request, res: Response, next: NextFunction) => {
-  config.fileServerHits++; 
-  next();
+  next()
 }
 
-const validateChirpHandler = (req: Request, res: Response) => {
-  let parsedBody: chirpBody;
+const middlewareMetricsInc = (req: Request, res: Response, next: NextFunction) => {
+  config.fileServerHits++
+  next()
+}
+
+const validateChirpHandler = async (req: Request, res: Response) => {
+  let parsedBody: chirpBody
 
   type chirpBody = {
-    body: string,
-  };
+    body: string
+  }
 
   type cleanedBody = {
-    cleanedBody: string,
-  };
+    cleanedBody: string
+  }
 
   type errorBody = {
-    error: string;  
-  };
+    error: string
+  }
 
   type validityBody = {
-    valid: boolean;
-  };
+    valid: boolean
+  }
 
-  parsedBody = req.body;
+  type invalidBody = {
+    invalid: boolean
+  } 
 
-  if (parsedBody.body.length <= 140) {
-    const keywords = ["kerfuffle", "sharbert", "fornax"];
-    
-    let words = parsedBody.body.split(" ");
-    for(let i = 0; i < words.length; i++) {
-      const lowered = words[i].toLowerCase();
-      if(keywords.includes(lowered)) {
-        words[i] = "****";
+  parsedBody = req.body
+
+    if (parsedBody.body.length <= 140) {
+      const keywords = ["kerfuffle", "sharbert", "fornax"]
+      
+      let words = parsedBody.body.split(" ")
+      for(let i = 0; i < words.length; i++) {
+        const lowered = words[i].toLowerCase()
+        if(keywords.includes(lowered)) {
+          words[i] = "****"
+        }
       }
-    }
 
-    const response: cleanedBody = {
-      cleanedBody: words.join(" "),
-    };
-    res.status(200).json(response);
+      const response: cleanedBody = {
+        cleanedBody: words.join(" ")
+      }
+      res.status(200).json(response)
 
-  } else {
-    const response: errorBody = {
-      error: "Chirp is too long",
-    };
-    res.status(400).json(response);
-
+    } else {
+      throw new badRequestError("Chirp is too long. Max length is 140")
   }
 }
 
-app.use("/app", middlewareMetricsInc, express.static("./src/app/"));
-app.use(middlewareLogResponses);
-app.use(express.json());
+app.use("/app", middlewareMetricsInc, express.static("./src/app/"))
+app.use(middlewareLogResponses)
 
 app.get("/app", (req, res) => {
-  res.set("Cache-Control", "no-cache");
-  res.send("Welcome to Chirpy");
-});
+  res.set("Cache-Control", "no-cache")
+  res.send("Welcome to Chirpy")
+})
 
 app.get("/api/healthz", (req, res) => { 
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.status(200).send("OK");
-}); 
+  res.set("Content-Type", "text/plain; charset=utf-8")
+  res.status(200).send("OK")
+})
 
 app.get("/admin/metrics", (req, res) => {
   res.set('Content-Type', 'text/html; charset=utf-8');
@@ -92,16 +130,24 @@ app.get("/admin/metrics", (req, res) => {
         <p>Chirpy has been visited ${config.fileServerHits} times!</p>
       </body>
     </html>
-  `);
-});
+  `)
+})
 
-app.post("/api/validate_chirp", validateChirpHandler);
+app.post("/api/validate_chirp", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await validateChirpHandler(req, res)
+  } catch(err) {
+    next(err)
+  }
+})
 
 app.post("/admin/reset", (req, res) => {
-  config.fileServerHits = 0;
-  res.status(200).end();
-});
+  config.fileServerHits = 0
+  res.status(200).end()
+})
+
+app.use(middlewareErrorHandler)
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
